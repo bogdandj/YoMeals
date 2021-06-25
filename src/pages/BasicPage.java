195462edcc6f933e419b6e3e11a29e78850c6101
bogdandj <vdjuric67@gmail.com>
2021-06-25 13:41:55 +0200

    set basic page

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,17 +1,19 @@
 package pages;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
-
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasicPage {
 
 	protected WebDriver driver;
+	protected WebDriverWait wait;
+	protected JavascriptExecutor js;
 	
+
 	public BasicPage(WebDriver driver) {
 		this.driver = driver;
+		wait = new WebDriverWait(driver, 10);
+		js = (JavascriptExecutor) driver;
 	}
 }
